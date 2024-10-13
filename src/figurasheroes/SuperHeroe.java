2f9fc63eab816593d209ca8eeeefd827ca16a710
wzseba <wzseba@gmail.com>
2024-10-13 07:07:22 -0300

    add: definicion de caracteristicas

--- src/figurasheroes/SuperHeroe.java
+++ src/figurasheroes/SuperHeroe.java
@@ -2,4 +2,43 @@ package figurasheroes;
 
 public class SuperHeroe {
 
+	private String nombre;
+	private String descripcion;
+	private boolean capa;
+
+	public SuperHeroe(String nombre) {
+		this.nombre = nombre;
+		this.descripcion = "";
+		this.capa = false;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public boolean isCapa() {
+		return capa;
+	}
+
+	public void setCapa(boolean capa) {
+		this.capa = capa;
+	}
+
+	@Override
+	public String toString() {
+		return "Super Heroe: " + nombre + ", descripcion: " + descripcion + ", capa: " + capa;
+	}
+
 }
